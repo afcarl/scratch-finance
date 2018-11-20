177c43551564258b26d9289ad9752dc4b56188e1
Shyrwinsteel Sia <shyrwin69@gmail.com>
2018-11-20 16:04:41 +0800

    Modified model and removed outliers

--- machine-learning/lstm/main.py
+++ machine-learning/lstm/main.py
@@ -2,6 +2,7 @@ from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.recurrent import LSTM
 from keras.models import Sequential
 from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
+import matplotlib.pyplot as plt
 
 from time import time
 import os, warnings
@@ -17,6 +18,16 @@ warnings.filterwarnings("ignore") # Hide messy Numpy warnings
 features = np.load('x_data.npy')
 targets = np.load('y_data.npy')
 
+# define three standard deviations
+std = 3
+
+# get the outliers indices
+outliers_indices = [i for i in range(len(targets)) if (abs(targets[i] - np.mean(targets)) > std*np.std(targets))]
+
+# delete them
+features = np.delete(features, outliers_indices, axis=0)
+targets = np.delete(targets, outliers_indices)
+
 # split the data 20% samples on test
 features_train, features_test, targets_train, targets_test = train_test_split(features, targets, test_size=0.20)
 
@@ -25,25 +36,22 @@ seq_len = 10
 params = 5
 epochs = 100
 batch_size = 512
-units = 32
-patience = 2
+units = 50
+patience = 3
 
 # build the model
 model = Sequential()
-model.add(LSTM(input_shape=(seq_len, params), output_dim=units, return_sequences=True))
-model.add(Dropout(0.2))
+model.add(LSTM(input_shape=(seq_len, params), output_dim=units, return_sequences=True, activation='relu'))
 model.add(LSTM(units, return_sequences=False, activation='relu'))
-model.add(Dropout(0.2))
 model.add(Dense(units, kernel_initializer='lecun_uniform', activation='relu'))
-model.add(Dropout(0.2))
 model.add(Dense(1, kernel_initializer='lecun_uniform', activation='linear'))
-model.compile(optimizer='adam',loss='mean_squared_error')
+model.compile(optimizer='adam',loss='mae', metrics=['mse','mae','mape','cosine'])
 model.summary()
 
 # declare the callbacks
 callbacks = [
-    EarlyStopping(monitor='val_loss', patience=patience),
-    ModelCheckpoint(filepath='best_model.h5', monitor='val_loss', save_best_only=True),
+    EarlyStopping(monitor='mae', patience=patience),
+    ModelCheckpoint(filepath='best_model.h5', monitor='mae', save_best_only=True),
     TensorBoard(log_dir="logs/{}".format(time()))]
 
 # train
@@ -51,17 +59,16 @@ history = model.fit(features_train, targets_train, epochs=epochs, callbacks=call
 
 # history
 #
-features_test.shape
-predicted = model.predict(features_test)
-predicted = np.reshape(predicted, (predicted.size,))
-predicted
-
-gg = pd.DataFrame(targets_test - predicted)
-gg.mean()
-gg.hist(alpha=0.8, bins=100)
-plt.show()
-
-model.save("model")
+# predicted = model.predict(features_test)
+# predicted = np.reshape(predicted, (predicted.size,))
+# predicted
+#
+# gg = pd.DataFrame(targets_test - predicted)
+# gg.mean()
+# gg.hist(alpha=0.8, bins=100)
+# plt.show()
+#
+# model.save("model")
 #
 # import pandas as pd
 # import matplotlib.pyplot as plt
